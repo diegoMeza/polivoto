839e45adf185a9e26fc7ae46f1cc3d721a79b6fe
Carlos Andrés Charris S <darkklitos@gmail.com>
2017-04-14 21:29:44 -0500

    configuración entorno más ágil

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,24 +1,14 @@
-<<<<<<< HEAD
-import { PoliVotoPage } from './app.po';
+import { PolivotoPage } from "./app.po";
 
-describe('poli-voto App', () => {
-  let page: PoliVotoPage;
-
-  beforeEach(() => {
-    page = new PoliVotoPage();
-=======
-import { PolivotoPage } from './app.po';
-
-describe('polivoto App', () => {
-  let page: PolivotoPage;
-
-  beforeEach(() => {
-    page = new PolivotoPage();
->>>>>>> Realizando cambios .gitignore
-  });
-
-  it('should display message saying app works', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
-  });
-});
+describe ( "polivoto App", () => {
+  let page : PolivotoPage;
+  
+  beforeEach ( () => {
+    page = new PolivotoPage ();
+  } );
+  
+  it ( "should display message saying app works", () => {
+    page.navigateTo ();
+    expect ( page.getParagraphText () ).toEqual ( "app works!" );
+  } );
+} );
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,15 +1,13 @@
-import { browser, element, by } from 'protractor';
+import { browser, by, element } from "protractor";
 
-<<<<<<< HEAD
-export class PoliVotoPage {
-=======
+
 export class PolivotoPage {
->>>>>>> Realizando cambios .gitignore
-  navigateTo() {
-    return browser.get('/');
+  
+  navigateTo () {
+    return browser.get ( "/" );
   }
-
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  
+  getParagraphText () {
+    return element ( by.css ( "app-root h1" ) ).getText ();
   }
 }
