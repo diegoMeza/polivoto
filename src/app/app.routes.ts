f6eaf084debf9248812c5b029d362f42ca112051
Carlos Andrés Charris S <darkklitos@gmail.com>
2017-04-14 22:04:12 -0500

    Adicionando Archivo de rutas de navegación

---
+++ src/app/app.routes.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by darkklitos on 14/04/17.
+ */
+
+import { RouterModule, Routes } from '@angular/router';
+
+const ROUTES : Routes = [
+  // { path: 'nombre_ruta', component: nombreComponent },
+  // { path: 'nombre_ruta/:id', component: nombreComponent },
+  // { path: '**', pathMatch: 'full', redirectTo: 'nombre_ruta' },
+];
+
+export const APP_ROUTING = RouterModule.forRoot ( ROUTES );
